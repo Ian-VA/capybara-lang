a5cb30db8780dbf82900654d54fcb3da125c2fad
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-06-22 15:18:47 -0700

    Update capybara-lang.cpp

--- capybara-lang.cpp
+++ capybara-lang.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <string>
 
+
+std::string evaluate(std::string input)
+{
+    return input;
+}
+
+std::string readln(std::string input)
+{
+    return input;
+}
+
+std::string out(std::string input)
+{
+    return input;
+}
+
+std::string rep(std::string input)
+{
+    auto read = readln(input);
+    auto res = evaluate(read);
+    return out(res);
+}
+
+
+int main()
+{
+    std::string input;
+    while (true) {
+        std::cout << "capybara> ";
+        if (!std::getline(std::cin, input)) {
+            break;
+        } else {
+            std::cout << rep(input) << "\n";
+        }
+    }
+}
