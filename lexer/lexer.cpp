faa9d2215904d7174cc7bf8c1ee4b0c53cee3343
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-06-25 21:12:08 -0700

    Update lexer.cpp

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -2,59 +2,55 @@
 #include <iostream>
 #include <string>
 #include <string_view>
-#include "header.hpp"
+#include "lexer.hpp"
+#include "utilfunctions.hpp"
 
 void lexer(std::string input)
 {
     int str_position = 0;
-    std::vector<Token> tokens = {};
+    std::vector<Token> tokens;
     std::string_view in = static_cast<std::string_view>(input);
 
-    while (str_position < in.length()){
-        
-        int startpos = str_position;
-        char ahead = in[startpos] + 1;
-        bool ispace = isspace(ahead);
-        bool isdigit = std::isdigit(ahead);
+    while (str_position < input.length()){
+        char currentpos = in[str_position];
+        bool ispace = isspace(currentpos);
+        bool isdigit = std::isdigit(currentpos);
+
+        if (currentpos == '+'){
+            tokens.push_back(Token{
+                    type::PLUS,
+                    '+'
+                });
+            str_position += 1;
+        } else if (currentpos == '*'){
+            tokens.push_back(Token{
+                    type::TIMES,
+                    '*'
+                });
+            str_position += 1;
+        }
 
         if (ispace){
             str_position += 1;
         } else if (isdigit){
-
+            str_position += 1;
         }
 
-        switch (ahead)
-        {
-            case '+':
-            {
-                str_position += 1;
-                Token token;
-                token.types = type::PLUS;
-                token.value = '+';
-                tokens.push_back(token);
-            }
-
-            case '*':
-            {
-                str_position += 1;
-                Token token;
-                token.types = type::TIMES;
-                token.value = '*';
-                tokens.push_back(token);
-            }
-
-        }
     }
-
-
 
     Token endinput;
     endinput.types = type::ENDINPUT;
     tokens.push_back(endinput);
 
+    print_tokens(tokens);
+
 }
+
 
 int main()
 {
-    
+    std::string input = "*+";
+    std::cout << input.length() << "\n";
+    lexer(input);
 }
+
