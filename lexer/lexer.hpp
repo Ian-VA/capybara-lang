25f21362facff24dd070faa774bc0dd5ee894a52
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-06-27 13:05:39 -0700

    Update lexer.hpp

--- lexer/lexer.hpp
+++ lexer/lexer.hpp
@@ -1,3 +1,6 @@
+#ifndef LEXER_HPP
+#define LEXER_HPP
+
 enum type
 {
     NUM,
@@ -5,15 +8,44 @@ enum type
     TIMES,
     FALSE,
     TRUE,
+    IDENTIFIER,
     LPAREN,
     RPAREN,
+    RSQBR,
+    LSQBR,
+    RCURLY,
+    LCURLY,
     FUNCTION,
     ENDINPUT
 };
 
+struct LEXER
+{
+    int l_paren_num = 0;
+    int r_paren_num = 0;
+    int rsq_br_num = 0;
+    int lsq_br_num = 0;
+    int rcurly = 0;
+    int lcurly = 0;
+    std::string l_error;
+    char ahead;
+    std::string l_location;
+};
 
 struct Token
 {
     type types;
     char value;
+
+    friend std::ostream& operator<<(std::ostream& os, const Token& tk);
 };
+
+std::ostream& operator<<(std::ostream& os, const Token& tk)
+{
+    os << " TOKEN VAL: " << tk.value << " TOKEN TYPE: " << tk.types << "\n";
+    return os;
+}
+
+
+#endif
+
