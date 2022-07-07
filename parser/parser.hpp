160016df8f1979beb023d3a8af34ba76007b066f
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-07-07 13:27:05 -0700

    Update parser.hpp

--- parser/parser.hpp
+++ parser/parser.hpp
@@ -1,9 +1,48 @@
-class AST 
+#include "classes.hpp"
+
+
+class LiteralNode
 {
-    
+    public:
+        LiteralNode(Token token){
+            if (!(token.types == type::NUM || token.types == type::IDENTIFIER)){
+                return;
+            }
+
+            this->literalnodetype = token.types;
+            this->value = token.value;
+        }
+    private:
+        std::string value;
+        type literalnodetype;
+
 };
 
-class LiteralNode : public AST
+class BinaryOperationNode
 {
+    LiteralNode left;
+    LiteralNode right;
+    Token operation;
+};
 
+class Parser 
+{
+    std::vector<Token> all_tokens;
+    int index = 1;
+
+    Token get_token()
+    {
+        if (index > all_tokens.size()){
+            return Token {type::ENDINPUT, "EOF"};
+        }
+        return all_tokens[index];
+    }
+
+    void advance(){
+        index += 1;
+    }
+
+    LiteralNode parseliteral(Token token){
+        return LiteralNode {token};
+    }
 };
