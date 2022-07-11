487e426f1d301cd7b831fda99c134b72a8c44d20
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-07-11 13:22:52 -0700

    Update parser.hpp

--- parser/parser.hpp
+++ parser/parser.hpp
@@ -18,19 +18,19 @@ struct LiteralNode
 
 struct BinaryOperationNode
 {
-    LiteralNode *left;
-    LiteralNode *right;
+    LiteralNode left;
+    LiteralNode right;
     Token operation;
 
     int evaluate(){
         switch (operation.types)
         {
             case type::PLUS:
-                return stoi(left->value) + stoi(right->value);
+                return stoi(left.value) + stoi(right.value);
             case type::TIMES:
-                return stoi(left->value) * stoi(right->value);
+                return stoi(left.value) * stoi(right.value);
             case type::SUBTRACT:
-                return stoi(left->value) - stoi(right->value);
+                return stoi(left.value) - stoi(right.value);
         }
     }
 };
@@ -69,37 +69,40 @@ struct Parser
         return LiteralNode {token};
     }
 
-    BinaryOperationNode parsebinary(Token token, LiteralNode *left, LiteralNode *right){
+    BinaryOperationNode parsebinary(Token token, LiteralNode left, LiteralNode right){
         return BinaryOperationNode {left, right, token};
     }
 
     template <class T>
-    T build_tree()
+    T parse()
     {
-        while (get_token().types != type::ENDINPUT)
+        switch(get_token().types)
         {
-            switch(get_token().types)
-            {
-                case type::PLUS:
-                case type::SUBTRACT:
-                case type::TIMES:
-                case type::DIVISION:
-                    if (get_last_token().types == type::NUM && peek_token().types == type::NUM){
-                        return BinaryOperationNode {
-                            get_token(),
-                            get_last_token(),
-                            peek_token(),
-                        };
-                        advance();
-                    } else {
-                        return ERROR {" ", "Incorrect use of operand" + get_last_token().value};
-                    }
-                case type::NUM:
-                case type::IDENTIFIER:
+            case type::PLUS:
+            case type::SUBTRACT:
+            case type::TIMES:
+            case type::DIVISION:
+                if (get_last_token().types == type::NUM && peek_token().types == type::NUM){
+                    return BinaryOperationNode {
+                        get_token(),
+                        LiteralNode {get_last_token().types, get_last_token().value},
+                        LiteralNode {peek_token().types, peek_token().value}
+                    };
+                    advance();
+                } else {
+                    return ERROR {" ", "Incorrect use of operand" + get_last_token().value};
+                    break;
+                }
+            case type::NUM:
+            case type::IDENTIFIER:
+                if (peek_token().types != (type::PLUS || type::SUBTRACT || type::TIMES || type::DIVISION)){
                     return LiteralNode {get_token().types, get_token().value};
-                    advance()
-
-            }
+                    advance();
+                } else {
+                    advance();
+                }
+            case type::ENDINPUT:
+                return Token {type::ENDINPUT, "EOF"};
         }
     }
 };
@@ -109,3 +112,4 @@ struct error
     std::string location;
     std::string type_of_error;
 };
+
