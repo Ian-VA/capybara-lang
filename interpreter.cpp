7064d329d1093e175ecc026d8b7a712df4325431
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-07-11 13:22:27 -0700

    Update interpreter.cpp

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,1 +1,17 @@
+#include "parser.hpp"
+#include "lexer.hpp"
 
+
+int main()
+{
+    std::string input = "5 + 3";
+    Parser parse;
+    parse.all_tokens = buid_all(input)
+
+    while (parse.parse() != Token {type::ENDINPUT, "EOF"} || parser.parse() != ERROR {})
+    {
+        parse.parse();
+    }
+
+    build_all(input);
+}
