4c4ea8a04c641d7c1161c69facead65f8dae162f
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-07-12 14:41:18 -0700

    Update parser.hpp

--- parser/parser.hpp
+++ parser/parser.hpp
@@ -10,7 +10,14 @@ struct LiteralNode
 
         this->literalnodetype = token.types;
         this->value = token.value;
-     }
+    }
+
+    std::string represent()
+    {
+        std::string rep = value;
+        return rep;
+    }
+    
     std::string value;
     type literalnodetype;
 
@@ -33,11 +40,19 @@ struct BinaryOperationNode
                 return stoi(left.value) - stoi(right.value);
         }
     }
+    
+    std::string represent()
+    {
+        std::string rep = left.value + operation.value + right.value;
+        return rep;
+    }
 };
 
 struct Parser 
 {
     std::vector<Token> all_tokens;
+    std::vector<BinaryOperationNode> all_operationnodes;
+    std::vector<LiteralNode> all_literalnodes;
     int index = 0;
 
     Token get_token()
@@ -65,44 +80,36 @@ struct Parser
         index += 1;
     }
 
-    LiteralNode parseliteral(Token token){
-        return LiteralNode {token};
-    }
-
-    BinaryOperationNode parsebinary(Token token, LiteralNode left, LiteralNode right){
-        return BinaryOperationNode {left, right, token};
-    }
-
-    template <class T>
-    T parse()
+    void parse()
     {
-        switch(get_token().types)
-        {
-            case type::PLUS:
-            case type::SUBTRACT:
-            case type::TIMES:
-            case type::DIVISION:
-                if (get_last_token().types == type::NUM && peek_token().types == type::NUM){
-                    return BinaryOperationNode {
-                        get_token(),
-                        LiteralNode {get_last_token().types, get_last_token().value},
-                        LiteralNode {peek_token().types, peek_token().value}
-                    };
-                    advance();
-                } else {
-                    return ERROR {" ", "Incorrect use of operand" + get_last_token().value};
-                    break;
-                }
-            case type::NUM:
-            case type::IDENTIFIER:
-                if (peek_token().types != (type::PLUS || type::SUBTRACT || type::TIMES || type::DIVISION)){
-                    return LiteralNode {get_token().types, get_token().value};
-                    advance();
-                } else {
-                    advance();
-                }
-            case type::ENDINPUT:
-                return Token {type::ENDINPUT, "EOF"};
+        while (get_token().value != "EOF"){
+            std::cout << "hi";
+            switch(get_token().types)
+            {
+                case type::PLUS:
+                case type::SUBTRACT:
+                case type::TIMES:
+                case type::DIVISION:
+                    if (get_last_token().types == type::NUM && peek_token().types == type::NUM){
+                        all_operationnodes.push_back(BinaryOperationNode {
+                            LiteralNode {get_last_token()},
+                            LiteralNode {peek_token()},
+                            get_token(),
+                        });
+                        advance();
+                    } else {
+                        std::cout << "Incorrect use of operand" << get_token().value;
+                        break;
+                    }
+                case type::NUM:
+                case type::IDENTIFIER:
+                    if (peek_token().types == (type::PLUS || type::SUBTRACT || type::TIMES || type::DIVISION)){
+                        all_literalnodes.push_back(LiteralNode {get_token()});
+                        advance();
+                    } else {
+                        advance();
+                    }
+            }
         }
     }
 };
