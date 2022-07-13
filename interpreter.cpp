dc000169c9da6c50bb95894d194eb837cce3417e
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-07-13 13:40:55 -0700

    Update interpreter.cpp

--- interpreter.cpp
+++ interpreter.cpp
@@ -6,11 +6,15 @@
 
 int main()
 {
-    std::string input = "5 + 3";
+    std::string input = "5 - 3";
     Parser parses;
     std::vector<Token> alltokens = build_all(input);
     parses.all_tokens = alltokens;
     parses.index = 0;
 
     parses.parse();
+
+    for (auto i : parses.all_operationnodes){
+        std::cout << i.evaluate() << "\n";
+    }
 }
