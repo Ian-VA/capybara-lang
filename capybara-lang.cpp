503a5d37f32d7851e807d18091194aca1056f98a
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-07-15 15:19:52 -0700

    Update capybara-lang.cpp

--- capybara-lang.cpp
+++ capybara-lang.cpp
@@ -4,12 +4,19 @@
 #include "test.hpp"
 
 
-
-std::string evaluate(std::string input)
-{
-    return input;
+int evaluate(BinaryOperationNode curr){
+    switch (curr.operation.types)
+    {
+        case type::PLUS:
+            return std::stoi(curr.left.value) + std::stoi(curr.right.value);
+        case type::TIMES:
+            return std::stoi(curr.left.value) * std::stoi(curr.right.value);
+        case type::SUBTRACT:
+            return std::stoi(curr.left.value) - std::stoi(curr.right.value);
+        case type::DIVISION:
+            return std::stoi(curr.left.value) / std::stoi(curr.right.value);
+    }
 }
-
 std::string readln(std::string input)
 {
     return input;
@@ -23,17 +30,20 @@ std::string out(std::string input)
 int rep(std::string input)
 {
     Parser parses;
+    int sum = 0;
     std::vector<Token> alltokens = build_all(input);
     parses.all_tokens = alltokens;
+
     parses.index = 0;
 
     parses.parse();
 
     for (auto i : parses.all_operationnodes){
-        return i.evaluate();
+        sum += evaluate(i);
     }
 
-    return 0;
+
+    return sum;
 }
 
 
