ff0ab57aa74e17a363373b760a9b711bbe15f91b
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-10-24 18:41:51 -0700

    refactoring, function calls added

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -1,25 +1,28 @@
 #include "parser.hpp"
 #include "lexer.hpp"
 #include <deque>
-// todo : type inference for variables, parse group expressions and other stuff, make cleaner error system, operator precedence
-std::unique_ptr<integerliteral> parserclass::parseInteger()
+
+std::unique_ptr<astnode> parserclass::parseInteger()
 {
     if (get_token().types != type::NUM){
         std::cout << "Expected integer, got " << get_token().value << "\n";
         return NULL;
     }
-    
-    return std::make_unique<integerliteral>(get_token());
+
+    auto result = std::make_unique<integerliteral>(get_token());
+    eat();
+    return std::move(result);
 }
 
-std::unique_ptr<variabledeclaration> parserclass::parseVariable()
+
+std::unique_ptr<astnode> parserclass::parseVariable()
 {
   if (get_token().types == type::VAR){
-      eat();
+      eat(); // eat var
       
       if (get_token().types != type::IDENTIFIER){
             std::cout << "Expected identifier after VAR, got " << get_token().value << "\n";
-            return NULL;
+            return nullptr;
       } else {
             std::string identifier = get_token().value;
             eat(); eat();
@@ -27,28 +30,34 @@ std::unique_ptr<variabledeclaration> parserclass::parseVariable()
             eat();
             return std::make_unique<variabledeclaration>(std::string("placeholder"), value, identifier);
       }
-  }  
+  } else if (get_token().types == type::IDENTIFIER) { // we then assume this was called by parseidentifiercall
+      std::string identifier = get_token().value;
+      eat(); eat();
+      std::string value = get_token().value;
+      eat();
+
+      return std::make_unique<variabledeclaration>(std::string("placeholder"), value, identifier);
+  }
+
 }
 
 std::unique_ptr<astnode> parserclass::parseOperation()
 {
     if (get_token().types == type::NUM){
-        std::unique_ptr<integerliteral> right = std::move(parseInteger());
-        eat();
+        std::unique_ptr<astnode> right = std::move(parseInteger());
         
         if (get_token().types != (type::PLUS || type::SUBTRACT || type::TIMES || type::DIVISION)){
             std::cout << "Expected operation, got " << get_token().value << "\n";
-            return NULL;
+            return nullptr;
         } else {
             std::string operation = get_token().value;
             eat();
             
             if (get_token().types != type::NUM){
                 std::cout << "Expected integer after: " << operation << " got " << get_token().value << " instead" << "\n";
-                return NULL;
+                return nullptr;
             } else {
-                std::unique_ptr<integerliteral> left = std::move(parseInteger());
-                eat();
+                std::unique_ptr<astnode> left = std::move(parseInteger());
                 
                 return std::make_unique<binaryoperation>(operation, std::move(right), std::move(left));
             }
@@ -57,14 +66,65 @@ std::unique_ptr<astnode> parserclass::parseOperation()
     } 
 }
 
-std::unique_ptr<astnode> parserclass::parseExpression()
+
+std::unique_ptr<astnode> parserclass::parseGroupedExpr()
 {
-    switch(get_token().types)
-    {
-        default:
-            return parseStatement();
+    eat();
+    auto V = parseExpression();
+
+    if (!V || get_token().value != ")") {
+        return nullptr;
+    }
+
+    eat();
+    return V;
+}
+
+std::unique_ptr<astnode> parserclass::parseIdentifierCall()
+{
+    std::string identifiername = get_token().value;
+    eat();
+
+    if (get_token().value != "(") {
+        return std::move(parseVariable());
+    } else {
+        eat(); // eat (
+        std::vector<std::unique_ptr<astnode>> args;
+
+        while (get_token().value != ")") {
+            if (auto arg = parseExpression()){
+                args.push_back(std::move(arg)); // push back arg
+
+                if (get_token().value != ",") {
+                    std::cout << "Expected ') or ',' token in argument list" << "\n";
+                    return nullptr;
+                } else {
+                    eat(); // eat ,
+                }
+            } else {
+                return nullptr;
+            }
+        }
+
+        eat(); // eat ')'
+        return std::make_unique<callfunctionnode>(identifiername, std::move(args));
     }
 }
+
+
+std::unique_ptr<astnode> parserclass::primaryParserLoop()
+{
+    switch (get_token().types)
+    {
+        case type::IDENTIFIER:
+            return std::move(parseIdentifierCall());
+        case type::NUM:
+            return std::move(parseInteger());
+        case type::LPAREN:
+            return std::move(parseGroupedExpr());
+    }
+}
+
 
 std::unique_ptr<astnode> parserclass::parseStatement()
 {
@@ -75,17 +135,17 @@ std::unique_ptr<astnode> parserclass::parseStatement()
 }
 
 
+
 int main()
 {
     parserclass parses;
-    std::string input = "var ten = 10";
+    std::string input = "function (arg1)";
     std::deque<Token> alltokens = build_all(input);
     parses.all_tokens = alltokens;
     parses.index = 0;
     std::vector<std::unique_ptr<astnode>> ast;
-    std::unique_ptr<variabledeclaration> test = std::move(parses.parseVariable());
+    std::unique_ptr<astnode> call = std::move(parses.parseIdentifierCall());
 
-    std::cout << std::move(test) << "\n";
 
     for (int i = 0; i < alltokens.size(); i++){
         switch (alltokens[i].types)
