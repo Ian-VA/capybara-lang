eba52596c79fd2999ea231aafddf58439094f788
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-11-02 18:54:11 -0700

    Update utilfunctions.hpp

--- lexer/utilfunctions.hpp
+++ lexer/utilfunctions.hpp
@@ -1,8 +1,27 @@
 #ifndef UTILFUNCTIONS_HPP
 #define UTILFUNCTIONS_HPP
 #include <vector>
-#include "lexer.hpp"
+#include "classes.hpp"
 #include <iostream>
+
+void print_tokens(std::vector<Token> all_tokens)
+{
+    std::cout << "Vector size: " << all_tokens.size() << '\n';
+    for (auto i : all_tokens)
+    {
+       std::cout << i;
+    }
+
+}
+
+bool isNumber(const std::string& s)
+{
+    for (char const &ch : s) {
+        if (std::isdigit(ch) == 0)
+            return false;
+    }
+    return true;
+}
 
 
 bool check_if_bool(Token token)
